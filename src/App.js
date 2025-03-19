e8418e234c39140c614d62cb097b0df86bfcbf82
Gourav Singh Panwar <gourav.panwar@avantika.edu.in>
2025-03-19 18:25:21 +0530

    Switch to HashRouter for better GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
@@ -13,7 +13,7 @@ import Login from './components/Login/Login';
 
 function App() {
   return (
-    <Router basename="/uiproject">
+    <Router>
       <div className="App">
         <Navbar />
         <Routes>
